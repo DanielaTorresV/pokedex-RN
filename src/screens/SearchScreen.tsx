06cf204b44d3a853097a0692d0da36a67b791235
Daniela Torres <dannytorres0211@gmail.com>
2022-11-10 10:03:02 -0500

    feat: create search input

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Platform, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import SearchInput from '../components/SearchInput';
 
 const SearchScreen = () => {
+
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={{ 
+      ...styles.container,
+      marginTop: Platform.OS === 'ios' ? top : top + 15
+      }}
+    >
+      <SearchInput />
     </View>
   )
 }
 
 export default SearchScreen;
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 20
+  }
+});
